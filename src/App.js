497fddc6c6ee7afb65c59b069a1d4190abbd60bf
DUSSARTLuke <lukedussart@hotmail.fr>
2021-04-15 09:08:36 +0200

    ajout scroll mouse

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
-import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import React, { useEffect } from "react";
+import {
+  Route,
+  Switch,
+  Redirect,
+  useLocation,
+  useHistory,
+} from "react-router-dom";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import { Project1, Project2, Project3, Project4 } from "./pages/Projects";
 
 const App = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+
+    const handleScrollToElement = (e) => {
+      const url = window.location.origin + "/";
+
+      const wheelRouter = (after, before) => {
+        if (e.wheelDeltaY < 0) {
+          setTimeout(() => {
+            history.push(after);
+          }, 500);
+        } else if (e.wheelDeltaY > 0) {
+          setTimeout(() => {
+            history.push(before);
+          }, 500);
+        }
+      }
+
+      switch (window.location.href.toString()) {
+        case url:
+          if (e.wheelDeltaY < 0) {
+            setTimeout(() => {
+              history.push("project-1");
+            }, 500);
+          }
+          break;
+        case url + "project-1":
+          wheelRouter("project-2", url);
+          break;
+        case url + "project-2":
+          wheelRouter("project-3", "project-1");
+          break;
+        case url + "project-3":
+          wheelRouter("project-4", "project-2");
+          break;
+        case url + "project-4":
+          wheelRouter("contact", "project-3");
+          break;
+        case url + "contact":
+          if (e.wheelDeltaY > 0) {
+             setTimeout(() => {
+               history.push("project-4");
+             }, 500);
+          }
+          break;
+        default:
+      }
+    };
+    
+    window.addEventListener("wheel", handleScrollToElement);
+  }, [history]);
+
   return (
     <div>
-      <Switch>
+      <Switch location={location} key={location.pathname}>
         <Route exact path="/" component={Home} />
         <Route exact path="/project-1" component={Project1} />
         <Route exact path="/project-2" component={Project2} />
